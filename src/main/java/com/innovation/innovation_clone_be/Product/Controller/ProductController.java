fd2bc12c82e47d705e4b1c078fdaa2a31f78d637
Jeeyeonn <qkrwldus3927@naver.com>
2022-09-12 00:57:27 +0900

    (메인,전체) 장바구니담기 구현 완료

--- src/main/java/com/innovation/innovation_clone_be/Product/Controller/ProductController.java
+++ src/main/java/com/innovation/innovation_clone_be/Product/Controller/ProductController.java
@@ -1,10 +1,12 @@
 package com.innovation.innovation_clone_be.Product.Controller;
 import com.innovation.innovation_clone_be.Error.Dto.ResponseDto;
+import com.innovation.innovation_clone_be.Product.Dto.Request.ProductRequestDto;
 import com.innovation.innovation_clone_be.Product.Service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequiredArgsConstructor
@@ -21,7 +23,7 @@ public class ProductController {
     // 장바구니에 해당 상품 담기 (상품전체페이지)
     //로그인 구현 후 HTTP 매개변수 넣기 --------------------------------------
     @PostMapping("/api/auth/cart")
-    public ResponseDto<?> addCartProduct(){
-
+    public ResponseDto<?> addCartProduct(@RequestBody ProductRequestDto requestDto){
+        return productService.addCartProduct(requestDto);
     }
 }
--- src/main/java/com/innovation/innovation_clone_be/Product/Service/ProductService.java
+++ src/main/java/com/innovation/innovation_clone_be/Product/Service/ProductService.java
@@ -1,6 +1,12 @@
 package com.innovation.innovation_clone_be.Product.Service;
 
+import com.innovation.innovation_clone_be.Cart.Entity.Cart;
+import com.innovation.innovation_clone_be.Cart.Repository.CartRepository;
 import com.innovation.innovation_clone_be.Error.Dto.ResponseDto;
+import com.innovation.innovation_clone_be.Error.Enum.ErrorCode;
+import com.innovation.innovation_clone_be.Member.Entity.Member;
+import com.innovation.innovation_clone_be.Member.Repository.MemberRepository;
+import com.innovation.innovation_clone_be.Product.Dto.Request.ProductRequestDto;
 import com.innovation.innovation_clone_be.Product.Dto.Response.ProductResponseDto;
 import com.innovation.innovation_clone_be.Product.Entity.Product;
 import com.innovation.innovation_clone_be.Product.Repository.ProductRepository;
@@ -16,6 +22,8 @@ import java.util.List;
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private final MemberRepository memberRepository;
+    private final CartRepository cartRepository;
 
     @Transactional
     public ResponseDto<?> getAllProduct() {
@@ -28,5 +36,29 @@ public class ProductService {
         }
 
         return ResponseDto.success(productResponseDtos);
+    }
+
+    public ResponseDto<?> addCartProduct(ProductRequestDto requestDto) {
+
+        //로그인 토큰 유효성 검증하기
+        //Member member = memberRepository.findById();
+        // 구현하기 ----------------------------------------------------------
+
+        Product product = productRepository.findProductById(requestDto.getProductId());
+
+        //해당하는 제품 ID가 없을 시 오류 코드 반환
+        if (product == null)
+            return ResponseDto.fail(ErrorCode.INVALID_PRODUCT);
+
+//        Cart cart = new Cart(member, product);
+//        cartRepository.save(cart);
+//
+//        member.getCarts().add(cart);
+
+        //해당 제품의 장바구니 수도 업데이트
+        product.setCartNum(product.getCarts().size());
+
+        return ResponseDto.success("success post");
+
     }
 }
