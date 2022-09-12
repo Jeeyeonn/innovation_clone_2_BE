9962b48b0f79f674de7e75f30ea73392908ee75e
petal416 <petal416@naver.com>
2022-09-12 21:38:55 +0900

    이메일, 비밀번호 정규식 추가

--- src/main/java/com/innovation/innovation_clone_be/Member/Dto/MemberRequestDto.java
+++ src/main/java/com/innovation/innovation_clone_be/Member/Dto/MemberRequestDto.java
@@ -4,8 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.*;
 
 @Getter
 @NoArgsConstructor
@@ -13,8 +12,10 @@ import javax.validation.constraints.NotNull;
 public class MemberRequestDto {
 
     @NotBlank
+    @Email(message = "Email 형식에 맞춰주세요.")
     private String email;
 
+    @Pattern(regexp = "[a-zA-Z1-9]{4,16}", message = "비밀번호는 영어와 숫자 포함해서 4~16자 사이로 입력해주세요.")
     @NotBlank
     private String password;
 
